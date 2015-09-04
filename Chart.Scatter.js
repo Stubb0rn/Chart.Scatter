508b463339924e88bbdd3dc689b62dde8d81273a
Boriss Nazarovs <bna@ambita.com>
2015-09-04 11:27:24 +0200

    Simplified x scale step calculation. Reduced density of steps on x scale

--- Chart.Scatter.js
+++ Chart.Scatter.js
@@ -512,80 +512,20 @@
 	chartjs.ScatterDateScale = chartjs.ScatterNumberScale.extend({
 
 		_calculateDateScaleRange: function (valueMin, valueMax, drawingSize, fontSize) {
+			var maxSteps = drawingSize / (fontSize * 12);
 
-			// todo: move to global object
-			var units = [
-				{ u: 1, c: 1, t: 1, n: 'ms' },
-				{ u: 1, c: 2, t: 2, n: 'ms' },
-				{ u: 1, c: 5, t: 5, n: 'ms' },
-				{ u: 1, c: 10, t: 10, n: 'ms' },
-				{ u: 1, c: 20, t: 20, n: 'ms' },
-				{ u: 1, c: 50, t: 50, n: 'ms' },
-				{ u: 1, c: 100, t: 100, n: 'ms' },
-				{ u: 1, c: 200, t: 200, n: 'ms' },
-				{ u: 1, c: 500, t: 500, n: 'ms' },
-				{ u: 1000, c: 1, t: 1000, n: 's' },
-				{ u: 1000, c: 2, t: 2000, n: 's' },
-				{ u: 1000, c: 5, t: 5000, n: 's' },
-				{ u: 1000, c: 10, t: 10000, n: 's' },
-				{ u: 1000, c: 15, t: 15000, n: 's' },
-				{ u: 1000, c: 20, t: 20000, n: 's' },
-				{ u: 1000, c: 30, t: 30000, n: 's' },
-				{ u: 60000, c: 1, t: 60000, n: 'm' },
-				{ u: 60000, c: 2, t: 120000, n: 'm' },
-				{ u: 60000, c: 5, t: 300000, n: 'm' },
-				{ u: 60000, c: 10, t: 600000, n: 'm' },
-				{ u: 60000, c: 15, t: 900000, n: 'm' },
-				{ u: 60000, c: 20, t: 1200000, n: 'm' },
-				{ u: 60000, c: 30, t: 1800000, n: 'm' },
-				{ u: 3600000, c: 1, t: 3600000, n: 'h' },
-				{ u: 3600000, c: 2, t: 7200000, n: 'h' },
-				{ u: 3600000, c: 3, t: 10800000, n: 'h' },
-				{ u: 3600000, c: 4, t: 14400000, n: 'h' },
-				{ u: 3600000, c: 6, t: 21600000, n: 'h' },
-				{ u: 3600000, c: 8, t: 28800000, n: 'h' },
-				{ u: 3600000, c: 12, t: 43200000, n: 'h' },
-				{ u: 86400000, c: 1, t: 86400000, n: 'd' },
-				{ u: 86400000, c: 2, t: 172800000, n: 'd' },
-				{ u: 86400000, c: 4, t: 345600000, n: 'd' },
-				{ u: 86400000, c: 5, t: 432000000, n: 'd' },
-				{ u: 604800000, c: 1, t: 604800000, n: 'w' }];
-
-			var maxSteps = drawingSize / (fontSize * 3.3);
-
-			var valueRange = +valueMax - valueMin,
-				offset = this.useUtc ? 0 : new Date().getTimezoneOffset() * 60000,
+			var offset = this.useUtc ? 0 : new Date().getTimezoneOffset() * 60000,
 				min = +valueMin - offset,
 				max = +valueMax - offset;
 
-			var xp = 0, f = [2, 3, 5, 7, 10];
-
-			while (valueRange / units[xp].t > maxSteps) {
-				xp++;
-
-				if (xp == units.length) {
-
-					var last = units[units.length - 1];
-					for (var fp = 0; fp < f.length; fp++) {
-						units.push({
-							u: last.u,
-							c: last.c * f[fp],
-							t: last.c * f[fp] * last.u,
-							n: last.n
-						});
-					}
-				}
-			}
-
-			var stepValue = units[xp].t,
-				start = Math.floor(min / stepValue) * stepValue,
-				stepCount = Math.ceil((max - start) / stepValue),
-				end = start + stepValue * stepCount;
+			var steps = Math.floor(maxSteps),
+				stepValue = (max - min) / steps,
+				end = min + stepValue * steps;
 
 			return {
-				min: start + offset,
+				min: min + offset,
 				max: end + offset,
-				steps: stepCount,
+				steps: steps,
 				stepValue: stepValue
 			};
 		},
